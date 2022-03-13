cd6d4084b734ad8d3cc06254ec91a87ae416e116
Miguel Borges <mrcb08022002@gmail.com>
2022-03-13 02:53:25 +0000

    boggers

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-fetch("perguntas/Web-Jonas - BQUI.csv").then(response => response.text()).then(data => main(data));
+let questions = [];
+const body = document.querySelector("#questions-container");
 
-function main(csv) {
+fetch("perguntas/Web-Jonas - BQUI.csv").then(response => response.text()).then(data => startup(data));
+
+function startup(csv) {
     csv = csv.split("\r\n");
     let splittedCsv = [];
     csv.forEach(string => splittedCsv.push(string.split(",")));
     delete csv;
-    let questions = [];
 
     for (let i = 1; i < splittedCsv.length; i++) {
         let question = {};
@@ -18,4 +20,53 @@ function main(csv) {
     }
 
     delete splittedCsv;
+    loadQuestions();
+}
+
+function loadQuestions() {
+    let question = questions[Math.floor(Math.random() * questions.length)]; 
+
+    let questionText = document.createElement('h1');
+    questionText.textContent =  question["Pergunta"];
+
+    let options = [];
+    let correctOption = document.createElement('div');
+    correctOption.textContent = question["Opção Correta"];
+    correctOption.dataset.isCorrect = "paraDeFazerBatota";
+    options.push(correctOption);
+
+    let wrongOptionOne = document.createElement('div');
+    wrongOptionOne.textContent = question["Opção Errada 1"];
+    options.push(wrongOptionOne);
+    
+    let wrongOptionTwo = document.createElement('div');
+    wrongOptionTwo.textContent = question["Opção Errada 2"];
+    options.push(wrongOptionTwo);
+    
+    let wrongOptionThree = document.createElement('div');
+    wrongOptionThree.textContent = question["Opção Errada 3"];
+    options.push(wrongOptionThree);
+    
+    options.forEach(option => { 
+        option.classList.add("option", "active")
+        option.addEventListener('click', clickHandler)
+    });
+    options.sort((_) => Math.random() - 0.5);
+    body.replaceChildren(questionText, ...options);
+
+    document.querySelector(".skip-question").addEventListener('click', loadQuestions);
+}
+
+function clickHandler(event) {
+    let options = document.querySelectorAll(".option");
+    options.forEach(option => { 
+        option.classList.remove("active");
+        option.removeEventListener('click', clickHandler);
+    });
+
+    if (event.target.dataset.isCorrect) event.target.style.backgroundColor = "var(--green)";
+    else {
+        event.target.style.backgroundColor = "var(--red)";
+        document.querySelector("[data-is-correct='paraDeFazerBatota']").style.backgroundColor = "var(--green)";
+    }
 }
